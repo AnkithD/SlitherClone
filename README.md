3f638d5c1e3a46593531257c19eaeeaa1eabeb4c
dhtikna <cs15btech11016@iith.ac.in>
2017-04-15 11:52:20 +0530

    Removed server debug messages

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SlitherClone
+
+--------------Install Instructions-------------
+1)Install Engo, according engo.io
+
+2)Also you may need the following dependencies:
+go get github.com/go-gl/mathgl
+go get github.com/golang/freetype
+go get golang.org/x/mobile/exp/audio/al
+
+Run the server.exe, on any computer run the client with:
+go run client.go
+
+use ipconfig to check the server's ip and enter it when the client asks
--- server.hs
+++ server.hs
@@ -8,6 +8,15 @@ splitString :: Eq a => a -> [a] -> [[a]]
 splitString x [] = []
 splitString x xs = p : splitString x (drop 1 q) where (p,q) = span (/= x) xs
 
+data Shape = Rectangle Float Float Float Float
+intersect :: Shape -> Shape -> Bool
+intersect (Rectangle x1 y1 x2 y2) (Rectangle x3 y3 x4 y4)
+       | x1 > x4 = False
+       | x2 < x3 = False
+       | y1 > y4 = False
+       | y2 < y3 = False
+       | otherwise = True
+
 main :: IO()
 main = do
     putStrLn "Started Server and Listing on Port: 7479..."
@@ -38,14 +47,14 @@ connHandler (sock1, _) (sock2, _) = do
 
 handleLoop :: Handle -> Handle -> IO()
 handleLoop handle1 handle2 = do
-    putStrLn "Waiting for Input 1"
+    --putStrLn "Waiting for Input 1"
     msg1 <- hGetLine handle1
-    putStrLn "Got Input 1: "
-    putStrLn msg1
-    putStrLn "Waiting for Input2"
+    --putStrLn "Got Input 1: "
+    --putStrLn msg1
+    --putStrLn "Waiting for Input2"
     msg2 <- hGetLine handle2
-    putStrLn "Got Input 2: "
-    putStrLn msg2
+    --putStrLn "Got Input 2: "
+    --putStrLn msg2
 
     if msg1 == "QUIT" then do
         hClose handle1
